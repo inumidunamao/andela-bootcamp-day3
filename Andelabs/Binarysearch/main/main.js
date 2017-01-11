e0057574fb07c812717fc1c98fe17c482beb6b39
Inumidun Amao <seindelawson@yahoo.com>
2017-01-11 17:45:50 +0100

    Updating binary search function and adding additional tests for edge cases

--- Andelabs/Binarysearch/main/main.js
+++ Andelabs/Binarysearch/main/main.js
@@ -26,6 +26,9 @@ function toOneThousand(){
 }
 
 function search(n) {
+	if(n === null ) return {count:null, index:null, length:this.length}
+	if(n === undefined ) return {count:undefined, index:undefined, length:this.length}
+	if (n.constructor != Number) return {count:0, index:-1, length:this.length}
 	var array = this.slice(),
 		first_term = 0,
 		last_term = array.length-1,
@@ -35,15 +38,20 @@ function search(n) {
 	} else if (array[last_term] === n){
 		return {count: 0, index:last_term, length:this.length}
 	} else {
-		while( first_term <= last_term){			
-			var half = Math.floor((first_term + last_term)/2);
-			
+		while( first_term <= last_term){
+			var long_cut = Math.floor((first_term + last_term)/4);
+			if (n === array[long_cut]){
+				return {count: count, index:long_cut, length:this.length}
+			} else if(n < array[long_cut]) {
+				var half = long_cut;	
+			} else {
+				var half = Math.floor((first_term + last_term)/2);
+			}
 			if (n === array[half]){
 				return {count: count, index:half, length:this.length}
 			}else if (array[first_term] === n) {
 				return {count: count, index:first_term, length:this.length}
 			} else if (array[last_term-1] === n){
-				
 				return {count: count, index:last_term-1, length:this.length}
 			}else if(half === 0 && array.length === 1){
 				return {count: count, index:-1, length:this.length}
